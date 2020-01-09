5aa5f0d418602b9f918e2b24371d1062299eb736
anam soomro <anamsoomroed@gmail.com>
2020-01-09 23:42:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,9 @@ def summon_captain_planet (array)
   result
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  #returns t or f if any words are >4 letters
+  words.any? {|x| x.length > 4}
 end
 
 def find_the_cheese# code an argument here
