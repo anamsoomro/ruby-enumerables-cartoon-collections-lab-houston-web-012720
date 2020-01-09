a2bfbb93f2cd2e6ab364a4f412a05739f33a7b31
anam soomro <anamsoomroed@gmail.com>
2020-01-09 23:47:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,5 @@ end
 def find_the_cheese(array)
   #go through array and reutrn the first cheese, if no cheese nil 
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.any? {|x| 
+  array.include {|x| 
 end
